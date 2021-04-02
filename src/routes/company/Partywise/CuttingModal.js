43b0e27fa7be252ca8138cdda86d6c4da40c4b76
sirisha <sirishabulusu2006@gmail.com>
2021-04-02 17:27:30 +0530

    cut instruction plan page weight rounded

--- src/routes/company/Partywise/CuttingModal.js
+++ src/routes/company/Partywise/CuttingModal.js
@@ -116,9 +116,9 @@ const CreateCuttingDetailsForm = (props) => {
     useEffect(() => {
         if(props.inward.process.length && props.inward.process.no) {
             if(props.coilDetails.instructionId)
-                props.setProcessDetails({...props.inward.process, weight: 0.00000000785*parseFloat(props.coilDetails.width)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no)});
+                props.setProcessDetails({...props.inward.process, weight:Math.round( 0.00000785*parseFloat(props.coilDetails.width)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no))});
             else
-                props.setProcessDetails({...props.inward.process, weight: 0.00000000785*parseFloat(props.inward.plan.fWidth)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no)});
+                props.setProcessDetails({...props.inward.process, weight:Math.round( 0.00000785*parseFloat(props.inward.plan.fWidth)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no))});
         }
     }, [props.inward.process.length, props.inward.process.no])
     
