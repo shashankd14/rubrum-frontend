f0f9d304517cbcd87de2a4a7e76303a0bfe3b2b4
sangeeta j <sanghu07@gmail.com>
2021-04-29 23:23:17 +0530

    weight calculation

--- src/routes/company/Partywise/CuttingModal.js
+++ src/routes/company/Partywise/CuttingModal.js
@@ -193,7 +193,7 @@ const CreateCuttingDetailsForm = (props) => {
         if(props.inward.process.length && props.inward.process.no) {
             if(props.coilDetails.instructionId)
 
-                props.setProcessDetails({...props.inward.process, weight:Math.round( 0.00000785*parseFloat(props.coilDetails.width)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no))});
+                props.setProcessDetails({...props.inward.process, weight:Math.round( 0.00000785*parseFloat(width)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no))});
             else
                 props.setProcessDetails({...props.inward.process, weight:Math.round( 0.00000785*parseFloat(props.inward.plan.fWidth)*parseFloat(props.inward.plan.fThickness)*parseFloat(props.inward.process.length)*parseFloat(props.inward.process.no))});
         }
