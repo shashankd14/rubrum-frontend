bf9b11291af7e0586577a24e26d0197fa8090f9d
sangeeta j <sanghu07@gmail.com>
2021-07-16 18:52:21 +0530

    deliveryDetails table

--- src/routes/company/Delivery/DeliveryDetails.js
+++ src/routes/company/Delivery/DeliveryDetails.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState}from "react";
 import {connect} from "react-redux";
-import { Descriptions, Input, Popover } from 'antd';
+import { Table } from 'antd';
 import { InfoCircleOutlined, CloseSquareTwoTone } from "@ant-design/icons";
 import {fetchDeliveryListById} from "../../../appRedux/actions";
 import moment from "moment";
@@ -9,6 +9,63 @@ import moment from "moment";
 const DeliveryDetails = (props) => {
   const [deliveryById, setDeliveryById]=useState(props.delivery.deliveryById);
   const [coil, setCoil] = useState(props.delivery.deliveryList);
+  const columns = [
+    {
+        title: 'Coil Number',
+        dataIndex: 'coilNumber',
+        key: 'x',       
+    },
+    {
+        title: 'Batch No',
+        dataIndex: 'customerBatchId',
+        key: 'customerBatchId'
+    },
+    {
+        title: 'Quantity',
+        dataIndex: 'actualWeight',
+        key: 'actualWeight'
+    },
+    {
+        title: 'Coil Width',
+        dataIndex:'plannedWidth',
+        
+    },
+    {
+        title: 'Coil Thickness',
+        dataIndex:'thickness',
+        
+    },
+    {
+        title: 'Coil length',
+        dataIndex: 'actualLength',
+        key: 'actualLength'
+    },
+    {
+      title: 'No of Pieces',
+      dataIndex: 'actualNoOfPieces',
+      key: 'actualNoOfPieces'
+  },
+    {
+        title: 'Material',
+        dataIndex: 'description',
+        key: 'description'
+    },
+    {
+      title: 'Grade',
+      dataIndex: 'grade',
+      key: ''
+    },
+    {
+      title: 'Packaging Classification',
+      dataIndex: 'packetClassification.classificationName',
+      key: 'packetClassification.classificationName'
+    },
+    {
+      title: 'Remarks',
+      dataIndex: 'remarks',
+      key: 'remarks'
+    },
+    ]
   useEffect(() => {
     if(props.delivery.deliveryList.length>0){
       let coilList =coil.filter(element => element.deliveryDetails.deliveryId == props.match.params.deliveryId);
@@ -17,7 +74,16 @@ const DeliveryDetails = (props) => {
 }, [])
 useEffect(() => {
   if(!props.delivery.loading && props.delivery.success) {
-    setDeliveryById(props.delivery.deliveryById);
+    let tableData = [];
+    tableData = props.delivery.deliveryById.map(element => {
+      element.coilNumber = coil.coilNumber;
+      element.customerBatchId = coil.customerBatchId;
+      // element.material = coil.material.description;
+      // element.thickness = coil.fThickness;
+      // element.grade = coil.grade.gradeName;
+      return element
+    })
+    setDeliveryById(tableData);
   }
 }, [props.delivery.loading, props.delivery.success])
   return (
@@ -26,62 +92,14 @@ useEffect(() => {
             <h3>Delivery Information</h3>
         </div>
        
-        {deliveryById.length > 0 ? deliveryById.map((elem) => (
-            <div key={elem.instructionId}
-              style={{
-                border: "1px solid black",
-                display: "flex",
-                padding: "5px 10px",
-                margin: "10px 0px",
-              }}
-
-            >
-              <div style={{ padding: "10px" }}>
-                <img
-                  src={require("assets/images/inward/cutting_icon.svg")}
-                  alt="main coil image"
-                  title="main coil image"
-                  style={{ marginTop: "10px" }}
-                />
-              </div>
-              <div className="flex flex-col">
-                <div style={{ marginTop: "5px" }}>
-                  <p style={{ fontWeight: "bold" }}><b>Coil Number -</b> {coil.coilNumber} </p>
-                </div>
-                <div
-                  style={{ display: "flex", justifyContent: "space-around" }}
-                >
-                  <div>
-                    <p style={{ marginTop: "10px" }}>
-                      <b>Batch No:</b>{coil.customerBatchId} |
-                    </p>
-                  </div>
-                  <div>
-                    <p style={{ marginTop: "10px" }}>
-                      <b>Quantity:</b>{elem.actualWeight !==null? elem.actualWeight: elem.plannedWeight} |
-                    </p>
-                  </div>
-                  <div>
-                    <p style={{ marginTop: "10px" }}>
-                      <b>Coil Width:</b>{elem.actualWidth !==null ? elem.actualWidth: elem.plannedWidth} |
-                    </p>
-                  </div>
-                  
-                  
-                  <div>
-                    <p style={{ marginLeft: "5px", marginTop: "10px" }}>
-                      <b>Coil Length:</b>{elem.actualLength !==null ? elem.actualLength: elem.plannedLength} |
-                    </p>
-                  </div>
-                  <div>
-                    <p style={{ marginLeft: "5px", marginTop: "10px" }}>
-                      <b>No Of Pieces:</b>{elem.actualNoOfPieces !==null ? elem.actualNoOfPieces: elem.plannedNoOfPieces} 
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )): <p> No Item to display for the selected delivery chalan Number</p>}
+        {deliveryById.length > 0 ? 
+          <Table
+          rowSelection={[]}
+             className="gx-table-responsive"
+             columns={columns}
+             dataSource={deliveryById}
+      />
+            : <p> No Item to display for the selected delivery chalan Number</p>}
       </div>
     
 
