214f0f923f4dfec955e4745a9de59ede7475241f
Madhuri Kashid <139782744+madhurikashid@users.noreply.github.com>
2024-02-05 22:15:06 +0530

    Revert to specific commit 2ec17c612f0c2596fea091a4014dca575dcb0915

--- src/routes/company/quality/reports/create/process/CuttingForm.js
+++ src/routes/company/quality/reports/create/process/CuttingForm.js
@@ -144,6 +144,10 @@ const CuttingForm = (props) => {
         }
       }, [props.templateDetails.operation]);
 
+    const instructionDate = props.templateDetails.packetDetails?.map(item=>item.instructionDate)
+
+    const [cutInspectionData, setCutInspectionData] = useState([])
+
     const [toleranceInspectionDataCut, settoleranceInspectionDataCut] = useState([])
     const [cutFormData, setCutFormData] = useState({
         processType: "cutting",
