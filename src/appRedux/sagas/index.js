aa7e146dad7b2b18db2d4546ca100997215096f3
Pragati Basa <pbasa@hollandamerica.com>
2025-01-09 10:56:01 +0530

    Removing firebase from codebase

--- src/appRedux/sagas/index.js
+++ src/appRedux/sagas/index.js
@@ -1,6 +1,4 @@
 import {all, fork} from "redux-saga/effects";
-import authSagas from "./Auth";
-import notesSagas from "./Notes";
 import inwardSagas from "./Inward";
 import partySagas from "./Party";
 import materialSagas from "./Material";
@@ -15,8 +13,6 @@ import labelPrintSaga from './LabelPrint';
 import YLRSagas from './YieldLossRatio';
 
 export default function* rootSaga() {
-  yield all([fork(authSagas)]);
-  yield all([fork(notesSagas)]);
   yield all([fork(inwardSagas)]);
   yield all([fork(partySagas)]);
   yield all([fork(materialSagas)]);
